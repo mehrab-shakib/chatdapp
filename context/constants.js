1432e2abdc3dcab7141c4329d7d7f26e2ab134f0
mehrab-shakib <fatscorpieeth@gmail.com>
2025-03-25 16:23:29 +0600

    All done

--- context/constants.js
+++ context/constants.js
@@ -1,9 +1,4 @@
-// 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
-// 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
-//  0x5FbDB2315678afecb367f032d93F642f64180aa3
-// 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+import chatDappJSON from "./ChatDapp.json";
+export const ChatDappAddress = "0x4977619Af0288BF04718eA63f69a26dB56CB3f71";
 
-import chatDappJSON from "./ChatDapp.json"
-export const ChatDappAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
-export const ChatDappABI = chatDappJSON.abi
+export const ChatDappABI = chatDappJSON.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require ("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_URL = process.env.SEPOLIA_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
   solidity: "0.8.28",
-  defaultNetwork: "localhost",
+  networks:{
+    sepolia: {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  }
+
+
+  
 };
